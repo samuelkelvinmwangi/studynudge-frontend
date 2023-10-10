193e97ad356c0de59c7ad7f52d0fcc98c25b46f7
samuel <samuelkelvinmwangi@gmail.com>
2023-10-10 11:07:24 +0300

    adding assets folder and fixing hero error

---
+++ src/components/HeroImg.js
@@ -0,0 +1,16 @@
+import "./HeroImgStyles.css";
+
+import React from 'react';
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+    <div className="mask">
+
+    </div>
+      
+    </div>
+  );
+}
+
+export default HeroImg;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import HeroImg from '../components/Heroimg';
 
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <HeroImg />
     </div>
   );
 }
