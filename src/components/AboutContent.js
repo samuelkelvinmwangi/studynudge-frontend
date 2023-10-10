901605f85c0143e51eb9f42df1c626fcd8984638
samuel <samuelkelvinmwangi@gmail.com>
2023-10-10 14:34:26 +0300

    adding content to the about page and about content

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,10 +1,24 @@
+import { Link } from "react-router-dom";
 import "./AboutContentStyles.css";
 
 import React from 'react';
+import Contact from '../routes/Contact'
 
 const AboutContent = () => {
   return (
-    <div>
+    <div className="about">
+    <div className="left">
+        <h1>About Us</h1>
+        <p>At Study Nudge, we are passionate about helping you achieve your goals and unlock your full potential. 
+        We understand that motivation is the driving force behind every success story. 
+        That's why we've created a platform dedicated to inspiring and empowering you on your journey towards greatness. 
+        Our team is committed to providing you with daily doses of inspiration, goal-setting tools, 
+        and a supportive community that will keep you motivated and on track.</p>
+        <Link to="/contact">
+            <button className="btn">Contact</button>
+        </Link>
+    </div>
+    <div className="right"></div>
       
     </div>
   );
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import AboutContent from '../components/AboutContent';
 
 const About = () => {
   return (
     <div>
     <Navbar/>
+    <AboutContent/>
       
     </div>
   );
