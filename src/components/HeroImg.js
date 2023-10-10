b7589d27316fcb7dc382351da0bce9e1aafdb32f
samuel <samuelkelvinmwangi@gmail.com>
2023-10-10 11:22:52 +0300

    adding img to the home page

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,14 +1,15 @@
 import "./HeroImgStyles.css";
 
 import React from 'react';
+import IntroImg from "../assets/Homepage.jpg"
 
 const HeroImg = () => {
   return (
     <div className="hero">
-    <div className="mask">
-
-    </div>
-      
+        <div className="mask">
+            <img className="home-img" 
+                src= {IntroImg} alt="IntroImg"/>
+        </div>
     </div>
   );
 }
