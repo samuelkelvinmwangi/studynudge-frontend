19b0c58a7ee8ad1f947ee2630fa40dc17e5e27cd
samuel <samuelkelvinmwangi@gmail.com>
2023-10-10 07:03:53 +0300

    adding routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./index.css"
+import Home from "./routes/Home"
+import About from "./routes/About"
+import Contact from "./routes/Contact"
 
 function App() {
   return (
-    <div>
-      <h1>this is my app</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
