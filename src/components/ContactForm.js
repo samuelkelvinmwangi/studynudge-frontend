926aea0d7145c61a4c56547bd15837cc774f8354
samuel <samuelkelvinmwangi@gmail.com>
2023-10-12 19:41:03 +0300

    adding icons and icon images to the contact page

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,10 +3,11 @@ import "./ContactFormStyles.css";
 
 
 import React from 'react';
+import { FaLocationArrow, FaMailBulk, FaPhone } from "react-icons/fa"
 
 const Contact = () => {
   return (
-    <div>
+    <div className="body">
       <h1>Contact Us</h1>
       <p>For any questions and login errors</p>
 
@@ -16,10 +17,22 @@ const Contact = () => {
             Contact Information
           </h4>
           <p>Our services are available and we have a dedicated team ready to assist you.</p>
+
           <div className="icon-text">
-            <i className="icon"></i>
+            <i className="icon"><FaPhone size={20} style={{color:"#fff", marginRight: "2rem"}}/></i>
             <span>text</span>
           </div>
+
+          <div className="icon-text">
+            <i className="icon"><FaMailBulk size={20} style={{color:"#fff", marginRight: "2rem"}}/></i>
+            <span>text</span>
+          </div>
+
+          <div className="icon-text">
+            <i className="icon"><FaLocationArrow size={20} style={{color:"#fff", marginRight: "2rem"}}/></i>
+            <span>text</span>
+          </div>
+
           <div className="social-media">
             <a href="#" className="icon-circle">
               <i className="icon"></i>
