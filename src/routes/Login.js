70d9d41a196f7f39bc33fbd5dc57b775d26a6bfe
samuel <samuelkelvinmwangi@gmail.com>
2023-10-13 12:12:17 +0300

    adding login page and js file

---
+++ src/routes/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import LoginForm from '../components/LoginForm';
+
+const Login = () => {
+  return (
+    <div>
+    <Navbar />
+    <LoginForm />
+      
+    </div>
+  );
+}
+
+export default Login;
