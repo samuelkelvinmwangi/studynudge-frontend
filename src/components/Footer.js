52263a79896468e343ff1e2bf9e341260deeef16
samuel <samuelkelvinmwangi@gmail.com>
2023-10-16 16:28:41 +0300

    finishing and making corrections in the footer component and css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,32 +10,27 @@ const Footer = () => {
 
             <div className="left">
                 <div className="location">
-                    <FaHome size={20} style={{color:'#000', marginRight:'2rem'}} />
+                    <FaHome size={20} style={{color:'#000', marginRight:'2rem'}} />Moringa School Ngong road
+                </div>           
+
+                <div className="phone">
+                     <FaPhone size={20} style={{color:'#000', marginRight:'2rem'}} />+254-712-456-789
                 </div>
-                <div>
-                    <p>Moringa School Ngong road.</p>
-                    <p>Kenya.</p>
+
+                <div className="email">
+                    <FaMailBulk size={20} style={{color:'#000', marginRight:'2rem'}} />snudge@gmail.com
                 </div>
             </div>
-
-            <div className="phone">
-            <FaPhone size={20} style={{color:'#000', marginRight:'2rem'}} />+254-712-456-789
-            </div>
-
-            <div className="email">
-            <FaMailBulk size={20} style={{color:'#000', marginRight:'2rem'}} />snudge@gmail.com
-            </div>
-
             <div className="right">
                 <h4>About Us</h4>
                 <p>At Study Nudge, we are passionate about helping you achieve your goals and unlock your full potential. 
-        We understand that motivation is the driving force behind every success story. 
-        That's why we've created a platform dedicated to inspiring and empowering you on your journey towards greatness.</p>
-            <div className="social">
-            <FaFacebook size={30} style={{color:'#000', marginRight:'1rem'}} />
-            <FaTwitter size={30} style={{color:'#000', marginRight:'1rem'}} />
-            <FaInstagram size={30} style={{color:'#000', marginRight:'1rem'}} />
-            </div>
+                We understand that motivation is the driving force behind every success story. 
+                That's why we've created a platform dedicated to inspiring and empowering you on your journey towards greatness.</p>
+                <div className="social">
+                    <FaFacebook size={30} style={{color:'#000', marginRight:'1rem'}} />
+                    <FaTwitter size={30} style={{color:'#000', marginRight:'1rem'}} />
+                    <FaInstagram size={30} style={{color:'#000', marginRight:'1rem'}} />
+                </div>
             </div>
 
         </div>
