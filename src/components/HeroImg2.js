61d2c2f089355b09eeb98047271b455c1d0fa586
samuel <samuelkelvinmwangi@gmail.com>
2023-10-16 20:23:55 +0300

    adding hero page 2 and css

--- src/components/HeroImg2.js
+++ src/components/HeroImg2.js
@@ -1,1 +1,18 @@
 import './HeroImg2Styles.css';
+
+
+import React from 'react';
+
+const HeroImg2 = () => {
+  return (
+    <div className='hero-img'>
+        <div className='heading'>
+            <hi>This is a heading</hi>
+            <p>This is a text</p>
+        </div>
+      
+    </div>
+  );
+}
+
+export default HeroImg2;
--- src/routes/Posts.js
+++ src/routes/Posts.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import PostsCards from '../components/PostsCards'
 import Footer from '../components/Footer';
+import HeroImg2 from '../components/HeroImg2';
 
 const Posts = () => {
   return (
     <div>
     <Navbar />
+    <HeroImg2 />
     <PostsCards />
     <Footer />
     </div>
