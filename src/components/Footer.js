e522638ae81c538c476c68f2393ccf108e17a1c2
samuel <samuelkelvinmwangi@gmail.com>
2023-10-16 15:47:46 +0300

    adding content to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 import "./FooterStyles.css"
 
 import React from 'react';
-import { FaHome, FaPhone } from "react-icons/fa"
+import { FaHome, FaMailBulk, FaPhone } from "react-icons/fa"
+import About from '../routes/About'
 
 const Footer = () => {
   return (
@@ -17,11 +18,21 @@ const Footer = () => {
                     <p>Kenya.</p>
                 </div>
             </div>
+
             <div className="phone">
-            <FaPhone size={20} style={{color:'#000', marginRight:'2rem'}} />
+            <FaPhone size={20} style={{color:'#000', marginRight:'2rem'}} />+254-712-456-789
             </div>
 
-            <div className="right"></div>
+            <div className="email">
+            <FaMailBulk size={20} style={{color:'#000', marginRight:'2rem'}} />snudge@gmail.com
+            </div>
+
+            <div className="right">
+                <h4>About Us</h4>
+                <p>At Study Nudge, we are passionate about helping you achieve your goals and unlock your full potential. 
+        We understand that motivation is the driving force behind every success story. 
+        That's why we've created a platform dedicated to inspiring and empowering you on your journey towards greatness.</p>
+            </div>
 
         </div>
     </div>
