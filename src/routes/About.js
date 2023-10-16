044b5fc057f70e9388ec2cbafa49ce1fd5cd2a68
samuel <samuelkelvinmwangi@gmail.com>
2023-10-16 21:53:28 +0300

    adding props and rendering component hero to the about page

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import AboutContent from '../components/AboutContent';
 import Footer from '../components/Footer';
+import HeroImg2 from './../components/HeroImg2';
 
 const About = () => {
   return (
     <div>
     <Navbar/>
+    <HeroImg2 heading="ABOUT US" text="Who We Are"/>
     <AboutContent/>
     <Footer />
       
