8dd372cfa57351aecdb87d2289aaa3832dfcceac
samuel <samuelkelvinmwangi@gmail.com>
2023-10-16 16:54:46 +0300

    adding footer component to the other components

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import AboutContent from '../components/AboutContent';
+import Footer from '../components/Footer';
 
 const About = () => {
   return (
     <div>
     <Navbar/>
     <AboutContent/>
+    <Footer />
       
     </div>
   );
--- src/routes/Posts.js
+++ src/routes/Posts.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import PostsCards from '../components/PostsCards'
+import Footer from '../components/Footer';
 
 const Posts = () => {
   return (
     <div>
     <Navbar />
     <PostsCards />
-      
+    <Footer />
     </div>
   );
 }
