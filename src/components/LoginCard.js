ded965de5a3ca02abd7ec3d08b837cddb0df7f44
samuel <samuelkelvinmwangi@gmail.com>
2023-10-17 09:18:15 +0300

    adding ligin card component

---
+++ src/components/LoginCard.js
@@ -0,0 +1,21 @@
+import './loginCardStyles.css';
+
+import React from 'react';
+
+const LoginCard = () => {
+  return (
+    <div className='login'>
+        <div className='card-container'>
+            <div className='card'>
+                <h3>Admin Login</h3>
+                <span className='bar'></span>
+                <p className='btc'>ADMIN</p>
+                <p></p>
+            </div>
+        </div>
+      
+    </div>
+  );
+}
+
+export default LoginCard;
--- src/routes/Posts.js
+++ src/routes/Posts.js
@@ -3,12 +3,14 @@ import Navbar from '../components/Navbar';
 import PostsCards from '../components/PostsCards'
 import Footer from '../components/Footer';
 import HeroImg2 from '../components/HeroImg2';
+import LoginCard from '../components/LoginCard';
 
 const Posts = () => {
   return (
     <div>
     <Navbar />
     <HeroImg2 heading="POSTS" text="Posts Section"/>
+    <LoginCard />
     <PostsCards />
     <Footer />
     </div>
