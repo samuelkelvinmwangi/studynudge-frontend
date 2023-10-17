6a6342d07085a4cf75f197ead1abf651c9ee1e38
samuel <samuelkelvinmwangi@gmail.com>
2023-10-17 12:14:21 +0300

    adding code tot post cards and css

--- src/components/PostsCards.js
+++ src/components/PostsCards.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './PostsCards.css';
+import post1 from '../assets/Video card 1.jpg';
 
 const PostsCards = () => {
   return (
-    <div>
-      
+    <div className='post-container'>
+      <h1 className='post-heading'>post</h1>
+      <div className='container-post'>
+        <div className='post-card'>
+          <img src={post1} alt='img1'/>
+          <h2 className='post-title'>post title</h2>
+          <div className='post-details'>
+            <p>details go here</p>
+          </div>
+        </div>
+      </div>     
     </div>
   );
 }
