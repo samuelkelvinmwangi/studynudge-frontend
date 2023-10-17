f4f3cf58a4a226a3973c8f0d197213b8f8bccbac
samuel <samuelkelvinmwangi@gmail.com>
2023-10-17 09:25:52 +0300

    adding card commponent changes

--- src/components/LoginCard.js
+++ src/components/LoginCard.js
@@ -1,17 +1,46 @@
 import './loginCardStyles.css';
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LoginCard = () => {
   return (
     <div className='login'>
         <div className='card-container'>
+
             <div className='card'>
                 <h3>Admin Login</h3>
                 <span className='bar'></span>
                 <p className='btc'>ADMIN</p>
-                <p></p>
+                <p>Add user</p>
+                <p>Remove post</p>
+                <p>Approve content</p>
+                <p>Create categories</p>
+                <Link to='/posts' className='btn'>Login Now</Link>
             </div>
+
+            <div className='card'>
+                <h3>Staff Login</h3>
+                <span className='bar'></span>
+                <p className='btc'>STAFF</p>
+                <p>Create profile</p>
+                <p>Edit content</p>
+                <p>Review content</p>
+                <p>Flag content</p>
+                <Link to='/posts' className='btn'>Login Now</Link>
+            </div>
+
+            <div className='card'>
+                <h3>Student Login</h3>
+                <span className='bar'></span>
+                <p className='btc'>STUDENT</p>
+                <p>subscribe</p>
+                <p>Post content</p>
+                <p>Comment</p>
+                <p>Add to wishlist</p>
+                <Link to='/posts' className='btn'>Login Now</Link>
+            </div>
+
         </div>
       
     </div>
