d60994c6bba26bec792d4b09d2c8e5cd8bb7a0a9
Brian Waweru <brianwaweru627@gmail.com>
2023-10-19 23:34:58 +0300

    Added content card elements without styling

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -2,7 +2,16 @@ import { useState } from 'react';
 
 function ContentCard() {
     return (
-        <div></div>
+        <div className='card-container'>
+            <div className='profile-pic-container'>
+                <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="Profile" />
+            </div>
+            <h3>dexterbrian</h3>
+            <h3>My Experience at the Safaricom Decode Event</h3>
+            <video src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" controls autoplay>
+                Your browser does not support the video tag.
+            </video>
+        </div>
     )
 }
 
