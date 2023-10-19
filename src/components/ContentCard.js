74a91a819b9a40f9c8628b6504fc3d055c93b8cc
Brian Waweru <brianwaweru627@gmail.com>
2023-10-19 23:55:56 +0300

    Added days ago and action icons without styling

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -6,11 +6,17 @@ function ContentCard() {
             <div className='profile-pic-container'>
                 <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="Profile" />
             </div>
-            <h3>dexterbrian</h3>
-            <h3>My Experience at the Safaricom Decode Event</h3>
-            <video src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" controls autoplay>
+            <h3 className='username'>dexterbrian</h3>
+            <h3>4 Days Ago</h3>
+            <h3 className='content-title'>My Experience at the Safaricom Decode Event</h3>
+            <video className='content-video' src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" controls autoplay>
                 Your browser does not support the video tag.
             </video>
+            <div>
+                <i class="fa-regular fa-comment"></i>
+                <i class="fa-regular fa-share-from-square"></i>
+                <i class="fa-regular fa-bookmark"></i>
+            </div>
         </div>
     )
 }
