6990a4f8ec4a4938cfd5ffae661c95c4e21fa587
Brian Waweru <brianwaweru627@gmail.com>
2023-10-19 23:01:50 +0300

    Added tabs without styling

--- src/components/ContentTab.js
+++ src/components/ContentTab.js
@@ -1,7 +1,32 @@
+import { useState } from 'react';
+
 function ContentTab() {
+    const [activeTab, setActiveTab] = useState('Videos');
+
+    const handleTabClick = (tab) => {
+        setActiveTab(tab);
+    };
+
     return (
         <div>
-            
+            <div className="tab-container">
+                <div className={`tab ${activeTab === 'Videos' ? 'active' : ''}`} onClick={() => handleTabClick('Videos')}>
+                    Videos
+                </div>
+                <div className={`tab ${activeTab === 'Audios' ? 'active' : ''}`} onClick={() => handleTabClick('Audios')}>
+                    Audios
+                </div>
+                <div className={`tab ${activeTab === 'Articles' ? 'active' : ''}`} onClick={() => handleTabClick('Articles')}>
+                    Articles
+                </div>
+            </div>
+
+            <div className="content-container">
+                {/* Render content based on activeTab */}
+                {activeTab === 'Videos' && <div>Video content goes here</div>}
+                {activeTab === 'Audios' && <div>Audio content goes here</div>}
+                {activeTab === 'Articles' && <div>Article content goes here</div>}
+            </div>
         </div>
     );
 }
