fbfdb9f81a38eea161a82378aefbe38c4332a67a
Brian Waweru <brianwaweru627@gmail.com>
2023-10-22 05:03:02 +0300

    Added ContentPage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./index.css"
+import ContentTab from "./components/ContentTab/ContentTab";
+import ContentCard from "./components/ContentCard/ContentCard";
+import ContentPage from "./pages/ContentPage";
 
 function App() {
   return (
     <div>
-      
+      <ContentPage />
     </div>
   );
 }
