f6d0904bc0ce371990eaaf51a4a46f66efd9cbdc
samuel <samuelkelvinmwangi@gmail.com>
2023-10-23 11:49:41 +0300

    making changes to the files

--- src/App.js
+++ src/App.js
@@ -5,15 +5,10 @@ import About from "./routes/About";
 import Contact from "./routes/Contact";
 import Login from "./routes/Login";
 import Posts from "./routes/Posts";
-import styled from 'styled-components';
-import Menu from './components/Menu';
 
 import { Route, Routes } from "react-router-dom";
-import Navbar2 from "./components/Navbar2";
 
-const Container = styled.div ;
-const Main = styled.div ;
-const Wrapper = styled.div ;
+
 
 
 function App() {
@@ -26,15 +21,6 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-      <Container>
-        <Menu />
-        <Main>
-          <Navbar2 />
-          <Wrapper>
-            
-          </Wrapper>
-        </Main>
-      </Container>
     </>
   );
 }
