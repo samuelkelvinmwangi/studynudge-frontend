d33b4a2b05e6d7944e672694f7d985427ad06b6c
samuel <samuelkelvinmwangi@gmail.com>
2023-10-23 22:41:25 +0300

    adding function for authentication to handle click action

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,6 +5,24 @@ import { useState} from 'react';
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    const response = awaiting fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (response.status === 200) {
+      const data = await response.json();
+      localStorage.setItem('token', data.token);
+      window.location.href = '/app';
+    } else {
+  
+    } 
+  }
 }
 
 const LoginForm = () => {
@@ -12,8 +30,8 @@ const LoginForm = () => {
   <div className='body-2' >
     <div className='cover'>
         <h1>Welcome Back</h1>
-        <input className='input' type='text' placeholder='username' />
-        <input className='input' type='password' placeholder='password' />
+        <input className='input' type='text' placeholder='username' value={username} onChange={(e) => setUsername(e.target.value)}/>
+        <input className='input' type='password' placeholder='password'  value={password} onChange={(e) => setPassword(e.target.value)}/>
 
         <div className='login-btn'>Login</div>
         <p className='paragraph'>By continuing you agree to our 
