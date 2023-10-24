130e05f4591be590b89d0fa9b5af271b048c28d0
Brian Waweru <brianwaweru627@gmail.com>
2023-10-24 15:38:06 +0300

    Created sampedata.js

---
+++ src/sampledata.js
@@ -0,0 +1,48 @@
+export const content = {
+    "videos": [
+      {
+        "title": "My Experience at the Safaricom Decode Event",
+        "url": "https://www.youtube.com/embed/3Q6Ntq9bQ4A",
+        "username": "johnsmith",
+        "created_at": "4 Days Ago"
+      },
+      {
+        "title": "How to make a website",
+        "url": "https://www.youtube.com/embed/3Q6Ntq9bQ4A",
+        "username": "janedoe",
+        "created_at": "4 Days Ago"
+      }
+    ],
+    "audios": [
+      {
+        "title": "Tech Talks with Experts - Episode 1",
+        "url": "https://example.com/podcast/episode1.mp3",
+        "username": "alexwilson",
+        "created_at": "5 Days Ago"
+      },
+      {
+        "title": "Tech Insights with Industry Leaders - Episode 2",
+        "url": "https://example.com/podcast/episode2.mp3",
+        "username": "emilyjones",
+        "created_at": "6 Days Ago"
+      }
+    ],
+    "articles": [
+      {
+        "title": "Introduction to React",
+        "url": "https://example.com/articles/react-intro",
+        "username": "michaelbrown",
+        "created_at": "7 Days Ago",
+        "content": "React is a JavaScript library for building user interfaces..."
+      },
+      {
+        "title": "Getting Started with Python",
+        "url": "https://example.com/articles/python-getting-started",
+        "username": "jessicasmith",
+        "created_at": "8 Days Ago",
+        "content": "Python is a versatile and powerful programming language..."
+      }
+    ]
+  };
+
+//gem install bundler:2.4.15
