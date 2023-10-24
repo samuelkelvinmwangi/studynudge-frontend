8037806bebefe1949b004c5d8d89da82451e89cb
Brian Waweru <brianwaweru627@gmail.com>
2023-10-24 14:14:09 +0300

    Hide pause button when mouse leaves the video player

--- src/components/ContentCard/ContentCard.js
+++ src/components/ContentCard/ContentCard.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './ContentCard.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMessage, faShareNodes, faBookmark, faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
@@ -7,6 +7,15 @@ function ContentCard() {
 
     const videoRef = useRef(null);
     const [isPlaying, setIsPlaying] = useState(false);
+    const [showPauseButton, setShowPauseButton] = useState(false);
+
+    const handleMouseEnter = () => {
+        setShowPauseButton(true);
+    };
+
+    const handleMouseLeave = () => {
+        setShowPauseButton(false);
+    };
 
     function handleVideo() {
         if (isPlaying) {
@@ -21,7 +30,7 @@ function ContentCard() {
     return (
         <div className='card-container'>
             <div className='profile-pic-container'>
-                <img className='profile-pic' src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="Profile Picture"/>
+                <img className='profile-pic' src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="Profile"/>
             </div>
             <div className='card-data-container'>
                 <div className='username-container'>
@@ -30,20 +39,27 @@ function ContentCard() {
                 </div>
                 <div className='content-data-container'>
                     <h5 className='content-title'>My Experience at the Safaricom Decode Event</h5>
-                    <div className='video-container'>
+                    <div 
+                        className='video-container' 
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
                         {
-                            isPlaying ? (
-                                <i id="pause-button" onClick={handleVideo}>
-                                    <FontAwesomeIcon icon={faPause} />
-                                </i>
-                            )
+                            isPlaying ? 
+                                showPauseButton ? (
+                                    <i id="pause-button">
+                                        <FontAwesomeIcon icon={faPause} onClick={handleVideo}/>
+                                    </i>
+                                ) : null
                             : (
-                                <i id="play-button" onClick={handleVideo}>
-                                    <FontAwesomeIcon icon={faPlay} />
+                                <i id="play-button">
+                                    <FontAwesomeIcon icon={faPlay} onClick={handleVideo}/>
                                 </i>
                             )
                         }
-                        <video ref={videoRef} className='content-video' src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4">
+                        <video
+                            ref={videoRef}
+                            className='content-video'
+                            src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4">
                             Your browser does not support the video tag.
                         </video>
                     </div>
