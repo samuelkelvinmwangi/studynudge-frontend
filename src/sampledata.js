eca675525546232363490c5074bab4318e47fd8d
Brian Waweru <brianwaweru627@gmail.com>
2023-10-24 16:22:54 +0300

    Updated sample data

--- src/sampledata.js
+++ src/sampledata.js
@@ -1,26 +1,37 @@
 export const content = {
     "videos": [
       {
+        "id": 1,
         "title": "My Experience at the Safaricom Decode Event",
-        "url": "https://www.youtube.com/embed/3Q6Ntq9bQ4A",
+        "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
         "username": "johnsmith",
-        "created_at": "4 Days Ago"
+        "created_at": "1 Days Ago"
       },
       {
+        "id": 2,
         "title": "How to make a website",
-        "url": "https://www.youtube.com/embed/3Q6Ntq9bQ4A",
+        "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
         "username": "janedoe",
+        "created_at": "2 Days Ago"
+      },
+      {
+        "id": 3,
+        "title": "Introduction to Docker",
+        "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+        "username": "dexterbrian",
         "created_at": "4 Days Ago"
       }
     ],
     "audios": [
       {
+        "id": 3,
         "title": "Tech Talks with Experts - Episode 1",
         "url": "https://example.com/podcast/episode1.mp3",
         "username": "alexwilson",
         "created_at": "5 Days Ago"
       },
       {
+        "id": 4,
         "title": "Tech Insights with Industry Leaders - Episode 2",
         "url": "https://example.com/podcast/episode2.mp3",
         "username": "emilyjones",
@@ -29,6 +40,7 @@ export const content = {
     ],
     "articles": [
       {
+        "id": 5,
         "title": "Introduction to React",
         "url": "https://example.com/articles/react-intro",
         "username": "michaelbrown",
@@ -36,6 +48,7 @@ export const content = {
         "content": "React is a JavaScript library for building user interfaces..."
       },
       {
+        "id": 6,
         "title": "Getting Started with Python",
         "url": "https://example.com/articles/python-getting-started",
         "username": "jessicasmith",
