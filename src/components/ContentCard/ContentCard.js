6954ac7047a95471d29d268d8228f4050a4e59f2
Brian Waweru <brianwaweru627@gmail.com>
2023-10-25 23:36:14 +0300

    Displaying ContentView page

--- src/components/ContentCard/ContentCard.js
+++ src/components/ContentCard/ContentCard.js
@@ -2,10 +2,12 @@ import { useState, useRef, useEffect } from 'react';
 import './ContentCard.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMessage, faShareNodes, faBookmark, faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from "react-router-dom";
 
-function ContentCard({ title, url, username, created_at, content = '', content_type }) {
+function ContentCard({ id, title, url, username, created_at, content = '', content_type }) {
 
     const mediaRef = useRef(null);
+    const navigate = useNavigate();
     const [isPlaying, setIsPlaying] = useState(false);
     const [showPauseButton, setShowPauseButton] = useState(false);
     const appUrl = process.env.PUBLIC_URL;
@@ -28,22 +30,32 @@ function ContentCard({ title, url, username, created_at, content = '', content_t
         }
     }
 
+    function openContentView() {
+        navigate(`/content/${id}`);
+    }
+
+    function openUserProfile() {
+        
+    }
+
     return (
         <div className='card-container'>
-            <div className='profile-pic-container'>
+            <div className='profile-pic-container' onClick={openUserProfile}>
                 <img className='profile-pic' src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="Profile"/>
             </div>
             <div className='card-data-container'>
                 <div className='username-container'>
-                    <h5 className='username'>{ username }</h5>
+                    <h5 className='username' onClick={openUserProfile}>{ username }</h5>
                     <h5 className='date'> { created_at }</h5>
                 </div>
                 <div className='content-data-container'>
-                    <h5 className='content-title'>{ title }</h5>
+                    <h5
+                        className='content-title'
+                        onClick={openContentView}>{ title }</h5>
                     {
                         content !== '' ? (
-                            <div className='image-container'>
-                                <img className='content-image' src={ url } alt="Content"/>
+                            <div className='image-container' onClick={openContentView}>
+                                <img className='content-image' src={ url } alt="Content" />
                             </div>
                         )
                         : (
@@ -67,8 +79,8 @@ function ContentCard({ title, url, username, created_at, content = '', content_t
                                 <video
                                     ref={mediaRef}
                                     className='content-media'
-                                    src={ content_type === 'audios' ? appUrl + url : url }
-                                    poster={ content_type === 'audios' ? 'https://img.freepik.com/free-vector/hand-drawn-flat-podcast-cover-template_23-2149433228.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1697846400&semt=ais' : ''}>
+                                    src={ content_type === 'audio' ? appUrl + url : url }
+                                    poster={ content_type === 'audio' ? 'https://img.freepik.com/free-vector/hand-drawn-flat-podcast-cover-template_23-2149433228.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1697846400&semt=ais' : ''}>
                                     Your browser does not support the video tag.
                                 </video>
                             </div>
