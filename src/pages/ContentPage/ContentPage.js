2adae3fb9d5b94663a617689633bfe045a7a76cc
Brian Waweru <brianwaweru627@gmail.com>
2023-10-26 02:20:38 +0300

    Inserted TopBar component in ContentPage.js

--- src/pages/ContentPage/ContentPage.js
+++ src/pages/ContentPage/ContentPage.js
@@ -1,9 +1,11 @@
 import Content from "../../components/Content/Content";
+import TopBar from "../../components/TopBar/TopBar";
 import './ContentPage.css';
 
 function ContentPage() {
     return (
         <div className="content-page-container">
+            <TopBar />
             <h2 className="page-title">Content</h2>
             <div className="content-tab-container">
                 <Content />
