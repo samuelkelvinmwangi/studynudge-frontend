f7965bdce6e49539458d256dba4eadb9c4a4ce0a
Brian Waweru <brianwaweru627@gmail.com>
2023-10-25 11:47:33 +0300

    Added routing for /, /content & /content/:id

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./index.css"
-import ContentTab from "./components/ContentTab/ContentTab";
-import ContentCard from "./components/ContentCard/ContentCard";
+import { Route, Routes } from "react-router-dom";
 import ContentPage from "./pages/ContentPage/ContentPage";
 
 function App() {
   return (
-    <div>
-      <ContentPage />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/content" element={<ContentPage />} />
+        <Route path="/content/:id" element={<h1>Content View</h1>} />
+      </Routes>
+    </>
   );
 }
 
