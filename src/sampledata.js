dafba89eed0ee2c718af9c2eb10a26a11e5600fb
Brian Waweru <brianwaweru627@gmail.com>
2023-10-25 19:59:19 +0300

    Updated sampledata.js

--- src/sampledata.js
+++ src/sampledata.js
@@ -1,51 +1,53 @@
 export const content = {
-    "videos": [
+    "content": [
       {
         "id": 1,
         "title": "My Experience at the Safaricom Decode Event",
         "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
         "username": "johnsmith",
-        "created_at": "1 Days Ago"
+        "created_at": "1 Days Ago",
+        "content_type": "videos"
       },
       {
         "id": 2,
         "title": "How to make a website",
         "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
         "username": "janedoe",
-        "created_at": "2 Days Ago"
+        "created_at": "2 Days Ago",
+        "content_type": "video"
       },
       {
         "id": 3,
         "title": "Introduction to Docker",
         "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
         "username": "dexterbrian",
-        "created_at": "4 Days Ago"
-      }
-    ],
-    "audios": [
+        "created_at": "4 Days Ago",
+        "content_type": "video"
+      },
       {
         "id": 3,
         "title": "Tech Talks with Experts - Episode 1",
         "url": "/audio/audio1.m4a",
         "username": "alexwilson",
-        "created_at": "5 Days Ago"
+        "created_at": "5 Days Ago",
+        "content_type": "audio"
       },
       {
         "id": 4,
         "title": "Tech Insights with Industry Leaders - Episode 2",
         "url": "/audio/audio2.m4a",
         "username": "emilyjones",
-        "created_at": "6 Days Ago"
-      }
-    ],
-    "articles": [
+        "created_at": "6 Days Ago",
+        "content_type": "audio"
+      },
       {
         "id": 5,
         "title": "Introduction to React",
         "url": "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         "username": "michaelbrown",
         "created_at": "7 Days Ago",
-        "content": "React is a JavaScript library for building user interfaces..."
+        "content": "React is a JavaScript library for building user interfaces...",
+        "content_type": "article"
       },
       {
         "id": 6,
@@ -53,9 +55,23 @@ export const content = {
         "url": "https://images.unsplash.com/photo-1600206287932-4b8888bd78f6?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         "username": "jessicasmith",
         "created_at": "8 Days Ago",
-        "content": "Python is a versatile and powerful programming language..."
+        "content": "Python is a versatile and powerful programming language...",
+        "content_type": "article"
       }
     ]
   };
 
 //gem install bundler:2.4.15
+
+// Create content
+// POST /contents
+// {
+//   "title": "Growing you Tech Career",
+//   "body": "To grow your tech career you have to make yourself in-demand...",
+//   "status": "pending",
+//   "category_id": 1,
+//   "user_id": 4,
+//   "content_urls": [
+//     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+//     ]
+// }
