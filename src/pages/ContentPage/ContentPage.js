12f33d6c6b3ec31f76249f57255a2ed458434d98
Brian Waweru <brianwaweru627@gmail.com>
2023-10-26 19:23:53 +0300

    Added NavigationBar component to ContentPage

--- src/pages/ContentPage/ContentPage.js
+++ src/pages/ContentPage/ContentPage.js
@@ -1,6 +1,7 @@
 import Content from "../../components/Content/Content";
 import TopBar from "../../components/TopBar/TopBar";
 import Modal from "../../components/Modal/Modal";
+import NavigationBar from '../../components/NavigationBar';
 import './ContentPage.css';
 import { useState } from 'react';
 
@@ -10,6 +11,7 @@ function ContentPage() {
 
     return (
         <div className="content-page-container">
+            <NavigationBar />
             <TopBar isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
             <div className="content-tab-container">
                 {
