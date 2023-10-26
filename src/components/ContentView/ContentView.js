d378d4d9a543c17d3775553696a2fc7999c3bcaa
Brian Waweru <brianwaweru627@gmail.com>
2023-10-26 19:24:08 +0300

    Added NavigationBar component to ContentView

--- src/components/ContentView/ContentView.js
+++ src/components/ContentView/ContentView.js
@@ -1,5 +1,6 @@
 import ContentCard from '../ContentCard/ContentCard';
 import { useParams, useLocation } from "react-router-dom";
+import NavigationBar from '../../components/NavigationBar';
 
 function ContentView() {
     
@@ -8,6 +9,7 @@ function ContentView() {
 
     return (
         <>
+            <NavigationBar />
             {
                 location.state ? (
                     <ContentCard title={location.state.title} url={location.state.url} username={location.state.username} created_at={location.state.created_at} content={location.state.content} content_type={location.state.content_type}/>
