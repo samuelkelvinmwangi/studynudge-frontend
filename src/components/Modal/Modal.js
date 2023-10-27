1971285e931e5935f5df304daac5756a0ac00d9c
Brian Waweru <brianwaweru627@gmail.com>
2023-10-27 15:28:36 +0300

    Enabled posting content

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Modal.css';
 
 function Modal({ isModalOpen = false, setIsModalOpen }) {
@@ -6,6 +6,7 @@ function Modal({ isModalOpen = false, setIsModalOpen }) {
   const [body, setBody] = useState('');
   const [category, setCategory] = useState('');
   const [file, setFile] = useState(null);
+  const [categories, setCategories] = useState([]);
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -28,18 +29,36 @@ function Modal({ isModalOpen = false, setIsModalOpen }) {
   };
 
   const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
+    setFile(event.target.value);
   };
 
   const handleSubmit = (event) => {
+    
     event.preventDefault();
-    // Handle form submission logic here
-    console.log('Title:', title);
-    console.log('Body:', body);
-    console.log('Category:', category);
-    console.log('File:', file);
+
+    const formData = {
+      title: title,
+      body: body,
+      category_id: category,
+      user_id: 1,
+      content_urls: [file]
+    };
+
+    fetch("https://snudgeapi.onrender.com/contents", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
     handleCloseModal();
   };
+
+  useEffect(() => {
+    fetch("https://snudgeapi.onrender.com/categories")
+      .then((r) => r.json())
+      .then(data => setCategories(data));
+  }, []);
 
   return (
     <div>
@@ -48,54 +67,62 @@ function Modal({ isModalOpen = false, setIsModalOpen }) {
       {isModalOpen && (
         <div className="modal">
           <div className="modal-content">
-            <span className="close" onClick={handleCloseModal}>
-              &times;
-            </span>
-            <h2>Create Content</h2>
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="title">Title:</label>
-              <input
-                type="text"
-                id="title"
-                name="title"
-                value={title}
-                onChange={handleTitleChange}
-                required
-              />
+            <div className='modal-header'>
+              <h2>Create Content</h2>
+              <span className="close" onClick={handleCloseModal}>
+                &times;
+              </span>
+            </div>
+            <div className='content-form'>
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  value={title}
+                  onChange={handleTitleChange}
+                  placeholder='Title'
+                  required
+                />
 
-              <label htmlFor="body">Body:</label>
-              <textarea
-                id="body"
-                name="body"
-                value={body}
-                onChange={handleBodyChange}
-                required
-              ></textarea>
+                <textarea
+                  id="body"
+                  name="body"
+                  value={body}
+                  onChange={handleBodyChange}
+                  placeholder='Description or article'
+                  required
+                ></textarea>
 
-              <label htmlFor="category">Category:</label>
-              <select
-                id="category"
-                name="category"
-                value={category}
-                onChange={handleCategoryChange}
-                required
-              >
-                <option value="">Select Category</option>
-                <option value="category1">Category 1</option>
-                <option value="category2">Category 2</option>
-                <option value="category3">Category 3</option>
-              </select>
+                <label htmlFor="category">Category:</label>
+                <select
+                  id="category"
+                  name="category"
+                  value={category}
+                  onChange={handleCategoryChange}
+                  required
+                >
+                  <option value="">Select Category</option>
+                  {categories.map((category) => (
+                    <option key={category.id} value={category.id}>
+                      {category.category_name}
+                    </option>
+                  ))}
+                </select>
 
-              <label htmlFor="fileUpload">File Upload:</label>
-              <input
-                type="file"
-                id="fileUpload"
-                name="fileUpload"
-                onChange={handleFileChange}
-              />
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  value={file}
+                  onChange={handleFileChange}
+                  placeholder='URL to the video or audio'
+                  required
+                />
 
-              <button type="submit">Create</button>
-            </form>
+                <button type="submit">Create</button>
+              </form>
+            </div>
           </div>
         </div>
       )}
