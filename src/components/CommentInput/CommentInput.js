fcaf20b9c6ca47199cfcd36b18a801a9091df486
Brian Waweru <brianwaweru627@gmail.com>
2023-10-29 23:10:18 +0300

    Moved CommentInput component & css file to components folder

---
+++ src/components/CommentInput/CommentInput.js
@@ -0,0 +1,32 @@
+import './CommentInput.css';
+import { apiUrl } from '../../apiUrl';
+
+function CommentInput() {
+
+    const [comment, setComment] = useState('');
+
+    function postComment() {
+
+        const commentData = {
+            user_id: 4,
+            comment: comment
+        };
+
+        fetch(`${apiUrl}/contents/${id}/comment`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(commentData),
+        });
+    }
+
+    return (
+        <div className="comment-input-container">
+            <input className="comment-input" type="text" placeholder="Add a comment..." value={comment} onChange={(e) => setComment(e.target.value)}/>
+            <button className="comment-button" onClick={postComment}>Comment</button>
+        </div>
+    );
+}
+
+export default CommentInput;
